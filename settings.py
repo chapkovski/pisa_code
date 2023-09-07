fcb308212807a4347a1ec47bb88f536ab8f6d0ec
Philipp Chapkovski <chapkovski@gmail.com>
2023-09-07 18:13:25 +0200

    adding rooms

--- settings.py
+++ settings.py
@@ -16,6 +16,7 @@ SESSION_CONFIGS = [
 SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
 )
+ROOMS= [{'name': 'pisa', 'display_name': 'Pisa'}]
 
 PARTICIPANT_FIELDS = []
 SESSION_FIELDS = []
