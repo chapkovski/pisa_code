12812870424810fa69c134a464676ececc87a26b
Philipp Chapkovski <chapkovski@gmail.com>
2024-05-29 18:43:11 +0200

    changing room name

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ SESSION_CONFIGS = [
 SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
 )
-ROOMS= [{'name': 'pisa', 'display_name': 'Pisa'}]
+ROOMS= [{'name': 'basel', 'display_name': 'Basel'}]
 
 PARTICIPANT_FIELDS = []
 SESSION_FIELDS = []
